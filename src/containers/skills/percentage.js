f65f1e9b79537983ae666ba4dd8ad8d35f5353ce
unknown <zidanalam77@gmail.com>
2024-11-04 10:40:04 +0700

    updated skilss

---
+++ src/containers/skills/percentage.js
@@ -0,0 +1,45 @@
+export const skillPercentage = [
+
+    {
+        label: "skill 1",
+        data: [{
+            skillName: "Python"
+        },
+        {
+            skillName: "MongoDB"
+        }
+        ]
+    },
+    {
+        label: "skill 2",
+        data: [{
+            skillName: "HTML & CSS"
+        },
+        {
+            skillName: "GO Language"
+        }
+        ]
+    },
+    {
+        label: "skill 3",
+        data: [{
+            skillName: "ReactJS"
+        },
+        {
+            skillName: "PhpMyAdmin"
+        }
+        ]
+    },
+    {
+        label: "Skill 5",
+        data: [
+            {
+                skillName: "3D Modelling",
+            },
+            {
+                skillName: "Git",
+            }]
+    }
+];
+
+
