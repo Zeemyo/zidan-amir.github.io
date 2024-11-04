c565f062c0707c53630b2440fdc4dee616c5b55a
unknown <zidanalam77@gmail.com>
2024-11-04 10:49:49 +0700

    update skills lagi

--- src/containers/skills/percentage.js
+++ src/containers/skills/percentage.js
@@ -1,33 +1,45 @@
 export const skillPercentage = [
 
     {
-        label: "skills",
+        label: "skill 1",
         data: [{
             skillName: "Python"
         },
         {
             skillName: "MongoDB"
-        },
-        {
+        }
+        ]
+    },
+    {
+        label: "skill 2",
+        data: [{
             skillName: "HTML & CSS"
         },
         {
             skillName: "GO Language"
-        },
-        {
+        }
+        ]
+    },
+    {
+        label: "skill 3",
+        data: [{
             skillName: "ReactJS"
         },
         {
             skillName: "PhpMyAdmin"
-        },
-        {
-            skillName: "3D Modelling",
-        },
-        {
-            skillName: "Git",
         }
         ]
     },
+    {
+        label: "Skill 5",
+        data: [
+            {
+                skillName: "3D Modelling",
+            },
+            {
+                skillName: "Git",
+            }]
+    }
 ];
 
 
